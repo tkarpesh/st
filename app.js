1b5211f9494573d2719c1f13d8f594476911d29d
Tatiana Karpesh <tatiana.karpesh@gmail.com>
2017-01-24 01:23:54 +0300

    Remove empty lines

--- app.js
+++ app.js
@@ -75,21 +75,3 @@ app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), () => {
   console.log(`Server started on port ${app.get('port')}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
