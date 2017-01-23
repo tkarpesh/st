e0401691784d4093d9d005ffe7c346d2e84b2d8b
Tatiana Karpesh <tatiana.karpesh@gmail.com>
2017-01-24 02:58:26 +0300

    Add sign up functionality

---
+++ models/user.js
@@ -0,0 +1,26 @@
+let mongoose = require('mongoose');
+let bcrypt = require('bcryptjs');
+
+let UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    index: true
+  },
+  password: {
+    type: String
+  },
+  email: {
+    type: String
+  }
+});
+
+let User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.createUser = (newUser, callback) => {
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      newUser.password = hash;
+      newUser.save(callback);
+    })
+  })
+}
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let router = express.Router();
 
+let User = require('../models/user');
+
 router.get('/sign_up', (req, res) => {
   res.render('sign_up');
 });
@@ -9,5 +11,37 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.post('/sign_up', (req, res) => {
+  let username = req.body.username;
+  let email = req.body.email;
+  let password = req.body.password;
+  let password2 = req.body.password2;
+
+  req.checkBody('username', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+  let errors = req.validationErrors();
+
+  if (errors) {
+    res.render('sign_up', { errors: errors })
+  } else {
+    let newUser = new User({
+      username: username,
+      email: email,
+      password: password
+    });
+
+    User.createUser(newUser, (err, user) => {
+      if (err) throw err;
+      console.log(user);
+    });
+
+    req.flash('success_msg', 'You are registered');
+    res.redirect('/users/login');
+  }
+});
 
 module.exports = router;
