817d2a9a87e9e0a7f4e75ccb756e6a53b577cfca
Tatiana Karpesh <tatiana.karpesh@gmail.com>
2017-01-24 01:25:20 +0300

    Fix --typo

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.set('view engine', 'handlebars');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.user(cookieParser());
+app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
