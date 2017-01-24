6999fc660b85a846a22a38a96f2769b9ae2abc4a
Tatiana Karpesh <tatiana.karpesh@gmail.com>
2017-01-25 02:33:53 +0300

    Clerefy login and logout

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', ensureAuthenticated, (req, res) => {
   res.render('index');
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    req.flash('error_msg', 'You are not logged in.');
+    res.redirect('/users/login');
+  }
+}
 
 module.exports = router;
