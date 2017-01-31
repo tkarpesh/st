19f50bca73df18d6e72b262619398c85bc23ccb6
Tatiana Karpesh <tatiana.karpesh@gmail.com>
2017-02-01 02:37:16 +0300

    Add user to article show page

--- routes/articles.js
+++ routes/articles.js
@@ -2,6 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 let Article = require('../models/article');
+let User = require('../models/user');
 
 router.get('/new', (req, res) => {
   res.render('articles/new', { user: req.user });
@@ -19,11 +20,13 @@ router.get('/:id', (req, res) => {
   let articleId = req.params.id;
 
   Article.findById(articleId, (err, article) => {
-    if (err) {
-      return res.redirect('articles/');
-    }
+    if (err) { return res.redirect('articles/'); }
 
-    res.render('articles/show', { article: article });
+    User.findById(article.userId, (err, user) => {
+      if (err) { return res.redirect('articles/'); }
+
+      res.render('articles/show', { article: article, user: user });
+    });
   });
 });
 
